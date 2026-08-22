236506a971324825e88c208c7f50e1de3cf49941
superranga013-ops <superranga013@gmail.com>
2026-08-22 12:41:10 +0530

    Refactor server.js to serve index.html directly

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => console.log(`Birthday site running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Shubha Birthday website running on port ${PORT}`);
+});
